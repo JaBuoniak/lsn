ccd645d717ce424c0b6a2c1b0b1bdc8498ba5d02
Paweł Jabłoński <pjablonski.elk@gmail.com>
2022-12-04 18:09:07 +0100

    Task1 allow multiline input

--- src/main/java/com/lsn/devtest/pj/Application.java
+++ src/main/java/com/lsn/devtest/pj/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        if (scanner.hasNextLine())
+        while (scanner.hasNextLine())
             System.out.println(Processor.process(scanner.nextLine()));
     }
 }
