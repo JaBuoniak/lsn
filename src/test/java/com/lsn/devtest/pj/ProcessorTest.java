40f0f89b3ae29b335c0c1cd175a97da0a4ec1a1d
Paweł Jabłoński <pjablonski.elk@gmail.com>
2022-12-04 18:06:41 +0100

    Task1 additional tests

--- src/test/java/com/lsn/devtest/pj/ProcessorTest.java
+++ src/test/java/com/lsn/devtest/pj/ProcessorTest.java
@@ -20,4 +20,34 @@ public class ProcessorTest {
         assertThat(outputLines[3]).isEqualTo("min: 1");
         assertThat(outputLines[4]).isEqualTo("max: 20");
     }
+
+    @Test
+    public void process_withNegatives() {
+        String input = "3 12 -20 21 -2 5 -2 2";
+
+        String output = Processor.process(input);
+
+        String[] outputLines = output.split("\n");
+        assertThat(outputLines.length).isEqualTo(5);
+        assertThat(outputLines[0]).isEqualTo("-20 -2 2 3 5 12 21");
+        assertThat(outputLines[1]).isEqualTo("count: 8");
+        assertThat(outputLines[2]).isEqualTo("distinct: 7");
+        assertThat(outputLines[3]).isEqualTo("min: -20");
+        assertThat(outputLines[4]).isEqualTo("max: 21");
+    }
+
+    @Test
+    public void process_withInvalidInput_shouldTakeOnlyIntegers() {
+        String input = "3 1.2 5,02 -2a 5 w2";
+
+        String output = Processor.process(input);
+
+        String[] outputLines = output.split("\n");
+        assertThat(outputLines.length).isEqualTo(5);
+        assertThat(outputLines[0]).isEqualTo("-2 1 2 3 5");
+        assertThat(outputLines[1]).isEqualTo("count: 8");
+        assertThat(outputLines[2]).isEqualTo("distinct: 5");
+        assertThat(outputLines[3]).isEqualTo("min: -2");
+        assertThat(outputLines[4]).isEqualTo("max: 5");
+    }
 }
