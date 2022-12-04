71fd9be40f455f7305de8c42a86402ced1b6a0f4
Paweł Jabłoński <pjablonski.elk@gmail.com>
2022-12-04 21:06:52 +0100

    Task2 additional tests

--- src/test/java/com/lsn/devtest/pj/ProcessorTest.java
+++ src/test/java/com/lsn/devtest/pj/ProcessorTest.java
@@ -18,4 +18,41 @@ public class ProcessorTest {
                 "6 7\n" +
                 "6 7");
     }
+
+    @Test
+    public void process_withNegatives() {
+        String input = "1 -2 10 7 15 7 3 -12 6 25 6 13 0";
+
+        String output = Processor.process(input);
+
+        assertThat(output).isEqualTo(
+                "-12 25\n" +
+                "-2 15\n" +
+                "0 13\n" +
+                "3 10\n" +
+                "6 7\n" +
+                "6 7");
+    }
+
+    @Test
+    public void process_withInvalidInput_shouldTakeOnlyIntegers() {
+        String input = "1.2 10,7 5e7x3 6.06 13 0.000";
+
+        String output = Processor.process(input);
+
+        assertThat(output).isEqualTo(
+                "0 13\n" +
+                "3 10\n" +
+                "6 7\n" +
+                "6 7");
+    }
+
+    @Test
+    public void process_withNoMatches() {
+        String input = "1 -2 11 7 14 8 3 -13 4 25 34 13 20";
+
+        String output = Processor.process(input);
+
+        assertThat(output).isEmpty();
+    }
 }
